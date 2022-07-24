d3b479fadfa8525cbdce128415c2d1880fc8976d
pcrow101 <34235799+pcrow101@users.noreply.github.com>
2022-07-24 16:12:47 +0100

    Fixed spin animation of correctly tapped flag

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -34,9 +34,9 @@ struct ContentView: View {
             RadialGradient(stops: [
                 .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
                 .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)
-
+                
             ], center: .top, startRadius: 200, endRadius: 700)
-                .ignoresSafeArea()
+            .ignoresSafeArea()
             VStack {
                 Spacer()
                 Text("Guess the Flag")
@@ -50,7 +50,7 @@ struct ContentView: View {
                         Text(countries[correctAnswer])
                             .font(.largeTitle.weight(.semibold))
                     }
-
+                    
                     ForEach(0..<3) { number in
                         Button(action: {
                             withAnimation {
@@ -59,46 +59,47 @@ struct ContentView: View {
                         }) {
                             FlagImage(text: self.countries[number])
                         }
-                        .rotation3DEffect(.degrees(number == correctAnswer ? animationAmount: 0), axis: (x: 0, y: 1, z: 0))
-                        .opacity(1.0)
+                        .rotation3DEffect(.degrees(number == correctAnswer ? animationAmount : 0), axis: (x: 0, y: 1, z: 0))
                     }
                     .frame(maxWidth: .infinity)
                     .padding(.vertical, 20)
                     .background(.regularMaterial)
                     .clipShape(RoundedRectangle(cornerRadius: 20))
-
-                Spacer()
-                Spacer()
-
-                Text("Score: \(score)")
-                    .foregroundColor(.white)
-                    .font(.title.bold())
-                Text("Taps left: \(tapsLeft)")
-                    .foregroundColor(.white)
-                    .font(.subheadline.bold())
-                Spacer()
+                    
+                    Spacer()
+                    Spacer()
+                    
+                    Text("Score: \(score)")
+                        .foregroundColor(.white)
+                        .font(.title.bold())
+                    Text("Taps left: \(tapsLeft)")
+                        .foregroundColor(.white)
+                        .font(.subheadline.bold())
+                    Spacer()
+                }
+                .padding()
             }
-            .padding()
-        }
-        .alert(scoreTitle, isPresented: $showingScore) {
-            Button("Continue", action: askQuestion)
-        } message: {
-            scoreTitle == "Correct" ?
-            Text("Your score is \(score)") :
-            Text("Wrong! That’s the flag of \(countries[incorrectFlag])")
-        }
-        .alert("Game Over", isPresented: $gameOver) {
-            Button("New Game", action: newGame)
-        } message: {
-            Text("You final score was \(score)")
+            .alert(scoreTitle, isPresented: $showingScore) {
+                Button("Continue", action: askQuestion)
+            } message: {
+                scoreTitle == "Correct" ?
+                Text("Your score is \(score)") :
+                Text("Wrong! That’s the flag of \(countries[incorrectFlag])")
+            }
+            .alert("Game Over", isPresented: $gameOver) {
+                Button("New Game", action: newGame)
+            } message: {
+                Text("You final score was \(score)")
+            }
         }
     }
-
+    
     func flagTapped(_ number: Int) {
-
+        
         if number == correctAnswer {
             scoreTitle = "Correct"
             score += 1
+            animationAmount += 360
         } else {
             scoreTitle = "Wrong"
             incorrectFlag = number
@@ -115,7 +116,7 @@ struct ContentView: View {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
     }
-
+    
     func newGame() {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
